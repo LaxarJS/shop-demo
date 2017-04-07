8fce66cc9d997d84d49c162fa30ab2bda3efed3b
Fredrik Jadebeck <fredrik.jadebeck@aixigo.de>
2017-04-07 13:24:50 +0200

    (#46) added nightwatch tests for saucelabs       based on PR (#48): https://github.com/LaxarJS/shop-demo/pull/48

---
+++ nightwatch-tests/conf/data/local.js
@@ -0,0 +1,14 @@
+module.exports = {
+   urls: {
+      imageRow3: 'bottle'
+   },
+   prices: {
+      'maxSubtotal': '€ 27.88',
+      'endSubtotal': '€ 0.99',
+      'cartRow1Price': '€ 0.99',
+      'cartRow2Price': '€ 25.90'
+   },
+   order: {
+      text: 'Thank you\nfor your order!'
+   }
+};
---
+++ nightwatch-tests/lib/pages/finish_order.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+   elements: {
+      order: {
+         selector: '#ax-headlineWidget-id6 > h2 > span'
+      },
+      continueShopping: {
+         selector: '#ax-headlineWidget-id6 > p > a'
+      }
+   }
+};
+
