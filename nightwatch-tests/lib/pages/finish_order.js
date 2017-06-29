dd964a138d4703b43d2d6ce236eaedbba44c9045
Michael Kurze <michael.kurze@aixigo.de>
2017-06-29 12:08:27 +0200

    (#54) added laxar-developer-tools-widget: fixed nightwatch tests some more

--- nightwatch-tests/lib/pages/finish_order.js
+++ nightwatch-tests/lib/pages/finish_order.js
@@ -3,10 +3,10 @@
 module.exports = {
    elements: {
       order: {
-         selector: '#ax-headlineWidget-id6 > h2 > span'
+         selector: '.headline-widget > h2 > span'
       },
       continueShopping: {
-         selector: '#ax-headlineWidget-id6 > p > a'
+         selector: '.headline-widget > p > a'
       }
    }
 };
