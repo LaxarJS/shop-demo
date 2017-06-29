9d901ed8aaf479d6e1f85420020e4b17e9a05f92
Michael Kurze <michael.kurze@aixigo.de>
2017-06-29 11:25:33 +0200

    (#54) added laxar-developer-tools-widget: fixed nightwatch tests

--- nightwatch-tests/lib/pages/shop_demo.js
+++ nightwatch-tests/lib/pages/shop_demo.js
@@ -13,60 +13,60 @@ module.exports = {
          selector: 'body > div > div.container > div > div'
       },
       articleBrowserWidget: {
-         selector: '#ax-articleBrowserWidget-id2'
+         selector: '.article-browser-widget'
       },
       tableTableHoverTableStripedAppArticles: {
-         selector: '#ax-articleBrowserWidget-id2 > div > table'
+         selector: '.article-browser-widget > div > table'
       },
       articleTeaserWidget: {
-         selector: '#ax-articleTeaserWidget-id3'
+         selector: '.article-teaser-widget'
       },
       shoppingCartWidget: {
-         selector: '#ax-shoppingCartWidget-id4'
+         selector: '.shopping-cart-widget'
       },
       appTeaserImage: {
-         selector: `#ax-articleTeaserWidget-id3 > div >
+         selector: `.article-teaser-widget > div >
             div.app-teaser-wrapper.clearfix.app-selection > div:nth-child(2) > div > img`
       },
       addToCartButton: {
-         selector: '#ax-articleTeaserWidget-id3 > div > div:nth-child(3) > button'
+         selector: '.article-teaser-widget > div > div:nth-child(3) > button'
       },
       cart: {
-         selector: '#ax-shoppingCartWidget-id4 > div > table'
+         selector: '.shopping-cart-widget > div > table'
       },
       cartRow1: {
-         selector: '#ax-shoppingCartWidget-id4 > div > table > tbody > tr:nth-child( 1 )'
+         selector: '.shopping-cart-widget > div > table > tbody > tr:nth-child( 1 )'
       },
       cartRow2: {
-         selector: '#ax-shoppingCartWidget-id4 > div > table > tbody > tr:nth-child( 2 )'
+         selector: '.shopping-cart-widget > div > table > tbody > tr:nth-child( 2 )'
       },
       cartRow1Price: {
-         selector: '#ax-shoppingCartWidget-id4 > div > table > tbody > tr > td:nth-child( 3 )'
+         selector: '.shopping-cart-widget > div > table > tbody > tr > td:nth-child( 3 )'
       },
       cartRow2Price: {
-         selector: '#ax-shoppingCartWidget-id4 > div >table > tbody > tr:nth-child( 2 ) > td:nth-child( 3 )'
+         selector: '.shopping-cart-widget > div >table > tbody > tr:nth-child( 2 ) > td:nth-child( 3 )'
       },
       cartSubtotal: {
-         selector: '#ax-shoppingCartWidget-id4 > div > table > tfoot > tr > td:nth-child( 3 )'
+         selector: '.shopping-cart-widget > div > table > tfoot > tr > td:nth-child( 3 )'
       },
       secondItemMinus: {
-         selector: `#ax-shoppingCartWidget-id4 > div > table > tbody >
+         selector: `.shopping-cart-widget > div > table > tbody >
             tr:nth-child(2) > td.app-increase-buttons > button:nth-child(2)`
       },
       secondItemPlus: {
-         selector: `#ax-shoppingCartWidget-id4 > div >table > tbody > tr:nth-child(2) >
+         selector: `.shopping-cart-widget > div >table > tbody > tr:nth-child(2) >
             td.app-increase-buttons > button:nth-child(1)`
       },
       firstItemMinus: {
-         selector: `#ax-shoppingCartWidget-id4 > div >table > tbody > tr:nth-child(1) >
+         selector: `.shopping-cart-widget > div >table > tbody > tr:nth-child(1) >
             td.app-increase-buttons > button:nth-child(2)`
       },
       firstItemPlus: {
-         selector: `#ax-shoppingCartWidget-id4 > div >table > tbody > tr:nth-child(1) >
+         selector: `.shopping-cart-widget > div >table > tbody > tr:nth-child(1) >
             td.app-increase-buttons > button:nth-child(1)`
       },
       orderButton: {
-         selector: '#ax-shoppingCartWidget-id4 > div > div > button'
+         selector: '.shopping-cart-widget > div > div > button'
       }
    }
 };
